f469be18c824ebd0c53264b25b0d89f3ed3531fc
Actually IAmPlayer <70600266+NotIAmPlayer@users.noreply.github.com>
2025-05-02 15:52:46 +0700

    go mod changes

--- controllers/userController.go
+++ controllers/userController.go
@@ -25,11 +25,13 @@ func hashPassword(password string) (string, error) {
 	return string(bytes), err
 }
 
+/*
 func comparePasswordHash(password string, hash string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 
 	return err == nil
 }
+*/
 
 func GetUsers(c *gin.Context) {
 	/*
