9b4fb5ce994fb168665a4051c0f080ed44679d7d
Actually IAmPlayer <70600266+NotIAmPlayer@users.noreply.github.com>
2025-06-02 14:47:06 +0700

    minor: customizable database with .env

--- config/database.go
+++ config/database.go
@@ -14,45 +14,44 @@ var DB *sql.DB
 func ConnectDB() {
 	err := godotenv.Load()
 	if err != nil {
-		log.Println("Error loading .env file")
+		log.Fatal("Error loading .env file")
+		return
 	}
 
 	username, exists := os.LookupEnv("DB_USERNAME")
 
 	if !exists {
-		log.Println("DB_USERNAME environment variable is not set")
+		log.Fatal("DB_USERNAME environment variable is not set")
 		return
 	}
 
 	password, exists := os.LookupEnv("DB_PASSWORD")
 
 	if !exists {
-		log.Println("DB_PASSWORD environment variable is not set")
+		log.Fatal("DB_PASSWORD environment variable is not set")
 		return
 	}
 
 	database, exists := os.LookupEnv("DB_DATABASE")
 
 	if !exists {
-		log.Println("DB_DATABASE environment variable is not set")
+		log.Fatal("DB_DATABASE environment variable is not set")
 		return
 	}
 
 	host, exists := os.LookupEnv("DB_HOST")
 
 	if !exists {
-		log.Println("DB_HOST environment variable is not set")
+		log.Fatal("DB_HOST environment variable is not set")
 		return
 	}
 
 	port, exists := os.LookupEnv("DB_PORT")
 
 	if !exists {
-		log.Println("DB_PORT environment variable is not set")
+		log.Fatal("DB_PORT environment variable is not set")
 		return
 	}
-
-	log.Println("username:", username, " password:", password, " database:", database, " host:", host, " port:", port)
 
 	DB, err = sql.Open("mysql", username+":"+password+"@tcp("+host+":"+port+")/"+database)
 
